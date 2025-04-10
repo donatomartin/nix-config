425c1c8b9e3e9ebc8829121fcaff2a0accdc2bba
donatomartin <uo288787@uniovi.es>
2025-04-11 01:20:59 +0200

    centralized config

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
+  let
+  	lib = nixpkgs.lib;
+	system = "x86_64-linux";
+	pkgs = nixpkgs.legacyPackages.${system};
+  in {
+
+    nixosConfigurations.nixos = lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [ ./configuration.nix ];
+    };
+
+    homeConfigurations.donato = home-manager.lib.homeManagerConfiguration {
+    	inherit pkgs;
+	modules = [ ./home.nix ];
+    };
+
+  };
+}
