57ef51751652eae22b40b55dd8bed0528c8a90a9
donatomartin <uo288787@uniovi.es>
2025-07-03 19:48:06 +0200

    feat(nixvim): remove unused logic

--- modules/nvim/nixvim-config.nix
+++ modules/nvim/nixvim-config.nix
@@ -91,15 +91,4 @@
 
   };
 
-  extraConfigLua = ''
-    vim.api.nvim_create_user_command("BOnly", function()
-      local current = vim.api.nvim_get_current_buf()
-      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-        if buf ~= current and vim.api.nvim_buf_is_loaded(buf) then
-          vim.api.nvim_buf_delete(buf, { force = true })
-        end
-      end
-    end, {})
-  '';
-
 }
