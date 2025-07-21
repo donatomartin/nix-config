75d3432722bdb3812c559bb8935ad17dd6675925
donatomartin <uo288787@uniovi.es>
2025-07-21 19:22:03 +0200

    feat: enable accessing harpoon files with numbers

--- modules/nvim/plugins/harpoon.nix
+++ modules/nvim/plugins/harpoon.nix
@@ -19,10 +19,15 @@
         "<leader>e" = "function() require'harpoon'.ui:toggle_quick_menu(require'harpoon':list()) end";
 
         # navigation
-        "<leader>h" = "function() require'harpoon':list():select(1) end";
-        "<leader>j" = "function() require'harpoon':list():select(2) end";
-        "<leader>k" = "function() require'harpoon':list():select(3) end";
-        "<leader>l" = "function() require'harpoon':list():select(4) end";
-        "<leader>ñ" = "function() require'harpoon':list():select(5) end";
+        "<leader>1" = "function() require'harpoon':list():select(1) end";
+        "<leader>2" = "function() require'harpoon':list():select(2) end";
+        "<leader>3" = "function() require'harpoon':list():select(3) end";
+        "<leader>4" = "function() require'harpoon':list():select(4) end";
+        "<leader>5" = "function() require'harpoon':list():select(5) end";
+        "<leader>6" = "function() require'harpoon':list():select(6) end";
+        "<leader>7" = "function() require'harpoon':list():select(7) end";
+        "<leader>8" = "function() require'harpoon':list():select(8) end";
+        "<leader>9" = "function() require'harpoon':list():select(9) end";
+        "<leader>0" = "function() require'harpoon':list():select(10) end";
       };
 }
