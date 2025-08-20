a41742332e470bab44448f9e50e53ac8e717e63b
donatomartin <uo288787@uniovi.es>
2025-08-20 20:59:45 +0200

    chore(nixvim): remove redundant home-manager entrypoint modules

--- modules/nvim/nixvim.nix
+++ modules/nvim/nixvim.nix
@@ -4,20 +4,6 @@
 
     imports = [
       ./nixvim-config.nix
-      ./plugins/neo-tree.nix
-      ./plugins/flash.nix
-      ./plugins/persistence.nix
-      ./plugins/conform.nix
-      ./plugins/cmp.nix
-      ./plugins/lsp.nix
-      ./plugins/lspsaga.nix
-      ./plugins/undotree.nix
-      ./plugins/mini.nix
-      ./plugins/treesitter.nix
-      ./plugins/telescope.nix
-      ./plugins/copilot.nix
-      ./plugins/gitsigns.nix
-      ./plugins/colorizer.nix
     ];
 
     defaultEditor = true;
