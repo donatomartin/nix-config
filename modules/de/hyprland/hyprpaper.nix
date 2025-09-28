ec9a3d884b3272c61410a62d58cab57dbb8e4f24
donatomartin <uo288787@uniovi.es>
2025-09-28 21:54:15 +0200

    feat: change hyprpaper config

--- modules/de/hyprland/hyprpaper.nix
+++ modules/de/hyprland/hyprpaper.nix
@@ -1,31 +1,18 @@
 { pkgs, ... }:
 let
-  wallpaperPath = builtins.toString ./../../../assets/wall1.png;
+  wallpaper = builtins.toString ./../../../assets/wall3.png;
 in
 {
   home.packages = with pkgs; [ hyprpaper ];
 
-  xdg.configFile."hypr/hyprpaper.conf".text = ''
-    	preload = ${wallpaperPath}
-    	wallpaper = ,${wallpaperPath}
-  '';
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      ipc = "on";
+      splash = false;
 
-  systemd.user.services.hyprpaper = {
-    Unit = {
-      Description = "Hyprpaper wallpaper daemon";
-      After = [ "hyprland-session.target" ];
-      PartOf = [ "hyprland-session.target" ];
-    };
-
-    Service = {
-      ExecStart = "${pkgs.hyprpaper}/bin/hyprpaper";
-      Restart = "on-failure";
-    };
-
-    Install = {
-      WantedBy = [ "hyprland-session.target" ];
+      preload = [ wallpaper ];
+      wallpaper = [ "eDP-1, ${wallpaper}" ];
     };
   };
-
-  systemd.user.startServices = true;
 }
