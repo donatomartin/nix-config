6d1c645c7d991a1c011d61714a9f4408b9cc253c
donatomartin <uo288787@uniovi.es>
2025-09-28 21:50:55 +0200

    feat: allow ghostty to pass M-[0-9] keybind

--- modules/de/ghostty.nix
+++ modules/de/ghostty.nix
@@ -9,6 +9,20 @@
       cursor-opacity = 0.5;
       font-size = 16;
       term = "xterm-256color";
+
+      # Alt+number → ESC+number
+      "keybind" = [
+        "alt+1=esc:1"
+        "alt+2=esc:2"
+        "alt+3=esc:3"
+        "alt+4=esc:4"
+        "alt+5=esc:5"
+        "alt+6=esc:6"
+        "alt+7=esc:7"
+        "alt+8=esc:8"
+        "alt+9=esc:9"
+        "alt+0=esc:0"
+      ];
     };
   };
 }
