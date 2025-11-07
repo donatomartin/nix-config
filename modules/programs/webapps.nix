7eb06b8ba17b9d116993e7acb2dd151ff1ea9d2c
donatomartin <uo288787@uniovi.es>
2025-11-07 16:36:32 +0100

    feat: remove word webapp

--- modules/programs/webapps.nix
+++ modules/programs/webapps.nix
@@ -8,14 +8,6 @@ let
     type = "Application";
     icon = ../../assets/webapp-icons/calendar.png;
   };
-  microsoft-word = pkgs.makeDesktopItem {
-    name = "Microsoft Word";
-    desktopName = "Microsoft Word";
-    exec = "brave --app=https://word.cloud.microsoft --ozone-platform-hint=wayland";
-    terminal = false;
-    type = "Application";
-    icon = ../../assets/webapp-icons/word.png;
-  };
   spotify = pkgs.makeDesktopItem {
     name = "Spotify";
     desktopName = "Spotify";
@@ -26,11 +18,8 @@ let
   };
 in
 {
-
   home.packages = [
     google-calendar
-    microsoft-word
     spotify
   ];
-
 }
