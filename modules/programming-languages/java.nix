6b8344babe5ffa16204db93f09df064f7d011d8c
donatomartin <uo288787@uniovi.es>
2026-01-17 10:56:24 +0100

    feat: remove unused java related packages

--- modules/programming-languages/java.nix
+++ modules/programming-languages/java.nix
@@ -4,13 +4,9 @@
     pkgs.temurin-bin-21
     pkgs.maven
     pkgs.gradle
-    pkgs.lombok
-    pkgs.jdt-language-server
-    pkgs.spring-boot-cli
   ];
 
   home.sessionVariables = {
     JAVA_HOME = "${pkgs.temurin-bin-21}";
-
   };
 }
