3415964c247ac5a862e12b24681213ee501238ff
donatomartin <uo288787@uniovi.es>
2026-01-17 10:57:45 +0100

    feat: remove terminal close confirmation

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -8,6 +8,7 @@
       cursor_color = "#CDD6F4";
       cursor_opacity = 0.5;           # note that this may need to be a string
       mouse_hide_while_typing = true;
+      confirm_os_window_close = 0;
     };
   };
 
