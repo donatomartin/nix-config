9512de4b969a3efd1f14ab09c3b54b7c1cbc882e
donatomartin <uo288787@uniovi.es>
2026-04-16 16:17:06 +0200

    feat(java): default to Temurin 21

--- modules/programming-languages/java.nix
+++ modules/programming-languages/java.nix
@@ -2,6 +2,8 @@
 {
   programs.java = {
     enable = true;
-    package = pkgs.temurin-bin-25;
+    package = pkgs.temurin-bin-21;
   };
+
+  home.file.".jdks/temurin-25".source = pkgs.temurin-bin-25;
 }
